ab44ba1d637d4f238fd965fe87fe8dd37018dce9
Joseph <jbaugh78@msn.com>
2019-03-23 16:38:07 -0500

    first 4 complete

---
+++ StudentExercise4Advanced/Data/Repository.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Linq;
+using  System.Collections.Generic;
+using StudentExercises4.Models;
+using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace StudentExercise4Advanced.Data
+{
+    class Repository
+    {
+        public SqlConnection Connection
+        {
+            get
+            {
+                string _connectionString =
+                    "Server=DESKTOP-ALG4281\\SQLEXPRESS;Database=StudentExercises2;Trusted_Connection=True;";
+                    return new SqlConnection(_connectionString);
+            }
+        }
+
+        //EXERCISES
+
+        public List<Exercise> GetAllExercises()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, ExerciseName, ExerciseLanguage FROM Exercise";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Exercise> exercises = new List<Exercise>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int exerciseNameColumnPosition = reader.GetOrdinal("ExerciseName");
+                        string exerciseNameValue = reader.GetString(exerciseNameColumnPosition);
+
+                        int exerciseLanguageColumnPosition = reader.GetOrdinal("ExerciseLanguage");
+                        string exerciseLanguageValue = reader.GetString(exerciseLanguageColumnPosition);
+                        Exercise exercise = new Exercise
+                        {
+                            Id = idValue,
+                            Name = exerciseNameValue,
+                            CodeLanguage = exerciseLanguageValue
+                        };
+                        exercises.Add(exercise);
+                    }
+                        reader.Close();
+                        return exercises;
+                }
+            }
+        }
+
+        //INSTRUCTORS 
+
+        public List<Instructor> GetAllInstructors()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText =
+                        "SELECT i.Id, i.FirstName, i.LastName, i.SlackHandle,c.Id AS CohortID, c.CohortName FROM Instructor i INNER JOIN Cohort c ON i.CohortId = c.id";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Instructor> instructors = new List<Instructor>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int firstNameColumnPosition = reader.GetOrdinal("FirstName");
+                        string instFirstNameValue = reader.GetString(firstNameColumnPosition);
+
+                        int lastNameColumnPosition = reader.GetOrdinal("LastName");
+                        string instLastNameValue = reader.GetString(lastNameColumnPosition);
+
+                        int slackHandleColumnPosition = reader.GetOrdinal("SlackHandle");
+                        string slackHandleValue = reader.GetString(lastNameColumnPosition);
+
+                        int cohoritIdColumnPosition = reader.GetOrdinal("CohortId");
+                        int cohortId = reader.GetInt32(cohoritIdColumnPosition);
+
+                        int cohortNameColumnPosition = reader.GetOrdinal("CohortName");
+                        string cohortName = reader.GetString(cohortNameColumnPosition);
+
+                        Cohort instructorsCohort = new Cohort
+                        {
+                            Id = cohortId,
+                            Name = cohortName
+                        };
+
+                        Instructor instructor = new Instructor(instFirstNameValue, instLastNameValue, slackHandleValue, instructorsCohort);
+
+                        instructors.Add(instructor);
+                    }
+                    reader.Close();
+                    return instructors;
+                }
+            }
+        }
+
+        //STUDENTS
+
+        public List<Student> GetAllStudentsBasic()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName, LastName, SlackHandle FROM Student";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Student> students = new List<Student>();
+
+                    while (reader.Read())
+                    {
+                        int firstNameColumnPosition = reader.GetOrdinal("FirstName");
+                        string firstNameValue = reader.GetString(firstNameColumnPosition);
+
+                        int lastNameColumnPosition = reader.GetOrdinal("LastName");
+                        string lastNameValue = reader.GetString(lastNameColumnPosition);
+
+                        int slackHandleColumnPosition = reader.GetOrdinal("SlackHandle");
+                        string slackHandleValue = reader.GetString(slackHandleColumnPosition);
+
+                        Student student = new Student
+                        {
+                            FirstName = firstNameValue,
+                            LastName = lastNameValue,
+                            SlackHandle = slackHandleValue,
+                        };
+
+                        students.Add(student);
+                    }
+                    reader.Close();
+                    return students;
+                }
+            }
+        }
+        //====================================================================================
+        //Cohort
+        //====================================================================================
+        public List<Cohort> GetallCohorts()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, CohortName FROM Cohort";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Cohort> cohorts = new List<Cohort>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int cohortNameColumnPosition = reader.GetOrdinal("CohortName");
+                        string cohortNameValue = reader.GetString(cohortNameColumnPosition);
+
+                        Cohort cohort = new Cohort
+                        {
+                            Id = idValue,
+                            Name = cohortNameValue
+                        };
+
+                        cohorts.Add(cohort);
+                    }
+                    reader.Close();
+                    return cohorts;
+                }
+            }
+        }
+    }
+}
---
+++ StudentExercise4Advanced/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StudentExercise4Advanced.Data;
+using StudentExercises4.Models;
+
+namespace StudentExercise4Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Choose a menu option:");
+                Console.WriteLine("1. Dispaly all Students.");
+                Console.WriteLine("2. Dispaly all Instructors.");
+                Console.WriteLine("3. Dispaly all Exercises.");
+                Console.WriteLine("4. Dispaly all Cohorts.");
+                Console.WriteLine("5. Search Students by last name.");
+                Console.WriteLine("6. Create New Cohort.");
+                Console.WriteLine("7. Create New Student.");
+                Console.WriteLine("8. Create New Instructor.");
+                Console.WriteLine("9. Display specific cohort students.");
+                Console.WriteLine("10. Move existing student to new cohort.");
+                Console.WriteLine("11. List specific students exercises.");
+                Console.WriteLine("11. Assign specific student specific exercise.");
+                Console.WriteLine("12. Exit.");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                string option = Console.ReadLine();
+
+
+                Repository repository = new Repository();
+                switch (option)
+                {
+                    case "1":
+
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("ALL STUDENTS---------------");
+
+                        List<Student> allStudentsBasicList = repository.GetAllStudentsBasic();
+                        int studentCounter = 0;
+                        foreach (Student student in allStudentsBasicList)
+                        {
+                            ++studentCounter;
+                            Console.WriteLine($"{studentCounter}: {student.FirstName} {student.LastName}");
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("ALL INSTRUCTORS ---------------------");
+                        List<Instructor> allInstructorsList = repository.GetAllInstructors();
+                        int instructorCounter = 0;
+                        foreach (Instructor instructor in allInstructorsList)
+                        {
+                            ++instructorCounter;
+                            Console.WriteLine($"{instructorCounter}: {instructor.FirstName} {instructor.LastName} is the instructor for {instructor.CohortNumber.Name}");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("ALL EXERCISES ---------------------");
+                        List<Exercise> exercises = repository.GetAllExercises();
+                        int exerciseCounter = 0;
+                        foreach (Exercise assignment in exercises)
+                        {
+                            ++exerciseCounter;
+                            Console.WriteLine($"{exerciseCounter}: {assignment.Name} using {assignment.CodeLanguage}");
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("ALL COHORTS ---------------------");
+                        List<Cohort> cohorts = repository.GetallCohorts();
+                        int cohortCounter = 0;
+                        foreach (Cohort cohort in cohorts)
+                        {
+                            ++cohortCounter;
+                            Console.WriteLine($"{cohortCounter}: {cohort.Name}");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Goodbye");
+                        break;
+                }
+                
+
+                /*
+                if (option == "1")
+                {
+                    Console.Write("What should I shout? ");
+                    string input = Console.ReadLine();
+                    Console.WriteLine(input.ToUpper() + "!!!");
+                }
+                else if (option == "2")
+                {
+                    Console.Write("What should I reverse? ");
+                    string input = Console.ReadLine();
+                    Console.WriteLine(new string(input.Reverse().ToArray()));
+                }
+                else if (option == "3")
+                {
+                    Console.WriteLine("Goodbye");
+                    break; // break out of the while loop
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid option: {option}");
+                }*/
+            }
+        }
+    }
+}
